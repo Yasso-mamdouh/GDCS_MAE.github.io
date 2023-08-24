5057e2d5db69934815412d1d0a51ff958935ed64
Yasser Mamdouh <105519472+Yasso-mamdouh@users.noreply.github.com>
2023-08-24 19:51:50 +0300

    Add files via upload

---
+++ Assignment-4/script.js
@@ -0,0 +1,10 @@
+let comapnyTitle = "Cisco Association", companyDescription = "It\'s the biggest company associated with network equipment and network software configrations", companyFoundationDate = "20/11/1997"
+let companyCard = `
+    <div>
+        <h3>${comapnyTitle}</h3>
+        <p>${companyDescription}</p>
+        <span>${companyFoundationDate}</span>
+    </div>
+`
+document.write(companyCard, companyCard, companyCard, companyCard)
+console.log("cisco")
